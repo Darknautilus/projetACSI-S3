8d306d817c0604bb00146527e0038156803ebd29
Aurélien Bertron <aure_bertron@hotmail.com>
2012-11-21 08:57:22 +0100

    Ajoute le projet pour les maquettes

---
+++ maquettes/src/main.java
@@ -0,0 +1,53 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+
+public class main {
+
+	protected Shell shlCmss;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			main window = new main();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shlCmss.open();
+		shlCmss.layout();
+		while (!shlCmss.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shlCmss = new Shell();
+		shlCmss.setBackground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_HIGHLIGHT_SHADOW));
+		shlCmss.setSize(504, 524);
+		shlCmss.setText("CMSS");
+
+	}
+}
